f0ce99b5bcab03a37d7a804725a12c82ee548ac9
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-11-03 19:23:14 -0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/pokemon/:id" component={PokeInfo} />
