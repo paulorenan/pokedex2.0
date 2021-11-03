01888744c4a27a3f89e8e0a311377be7f517480b
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-11-03 17:19:08 -0300

    Fazendo css PkeInfo

---
+++ src/components/Ability.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import { getPoke } from '../services/pokeapi.js'
+
+function Ability(props) {
+  const { ability } = props.ability
+  const [pokeAbility, setAbility] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    async function fetchData() {
+      const pokemon = await getPoke(ability.url)
+      setAbility(pokemon)
+      setLoading(false)
+    }
+    fetchData()
+    console.log(pokeAbility);
+  }, [ability.url])
+
+  function capitalize(string) {
+    const maiuscula = string.map((palavra)  => (
+      palavra.charAt(0).toUpperCase() + palavra.slice(1)
+      ))
+    return maiuscula.join(' ')
+  }
+
+  function filtrarAbilities(abilities) {
+    const array = abilities.effect_entries
+    const arrayFiltrado = array.filter((item) => item.language.name === 'en')
+    return arrayFiltrado[0].short_effect
+  }
+
+  return (
+    <div>
+      {!loading && 
+      <details>
+        <summary>{capitalize(ability.name.split('-'))}</summary>
+        <p>{filtrarAbilities(pokeAbility)}</p>
+      </details>}
+    </div>
+  )
+}
+
+export default Ability
