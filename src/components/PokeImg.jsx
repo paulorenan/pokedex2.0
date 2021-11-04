2b2170977be23890b127fa7c11ca1ce8841a8b50
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-11-04 18:07:41 -0300

    Reanja os sprites

--- src/components/PokeImg.jsx
+++ src/components/PokeImg.jsx
@@ -6,81 +6,85 @@ function PokeImg(props) {
     <div className="imgCont">
       <h3>Sprites</h3>
       <div className="img1">
-        <div className="img2">
-          <div className="nomePok">
-            <p className="lado">Frente</p>
+        <div className="img3">
+          <div className="img2">
+            <div className="nomePok">
+              <p className="lado">Frente</p>
+            </div>
+            <div>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
           </div>
-          <div>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          {pokemon.sprites.back_default !== null &&
+          <div className="img2">
+            <div className="nomePok">
+              <p className="lado">Costas</p>
+            </div>
+            <div>
+            <img src={pokemon.sprites.back_default} alt={pokemon.name} />
+            </div>
+          </div>}
+          <div className="img1">
+          <div className="img2">
+            <div className="nomePok">
+              <p className="lado">{"Frente (Shiny)"}</p>
+            </div>
+            <div>
+            <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
+            </div>
           </div>
+          {pokemon.sprites.back_shiny !== null && 
+          <div className="img2">
+            <div className="nomePok">
+              <p className="lado">{"Costas (Shiny)"}</p>
+            </div>
+            <div>
+            <img src={pokemon.sprites.back_shiny} alt={pokemon.name} />
+            </div>
+          </div>}
         </div>
-        {pokemon.sprites.back_default !== null &&
-        <div className="img2">
-          <div className="nomePok">
-            <p className="lado">Costas</p>
-          </div>
-          <div>
-          <img src={pokemon.sprites.back_default} alt={pokemon.name} />
-          </div>
-        </div>}
       </div>
-      {pokemon.sprites.back_female != null && 
-        <div className="img1">
-          <div className="img2">
-            <div className="nomePok">
-              <p className="lado">{'Frente (Fêmea)'}</p>
-            </div>
-            <div>
-              <img src={pokemon.sprites.front_female} alt={pokemon.name} />
-            </div>
-          </div>
-          <div className="img2">
-            <div className="nomePok">
-              <p className="lado">{"Costas (Fêmea)"}</p>
-            </div>
-            <div>
-              <img src={pokemon.sprites.back_female} alt={pokemon.name} />
-            </div>
-          </div>
-      </div> }
-      <div className="img1">
-        <div className="img2">
-          <div className="nomePok">
-            <p className="lado">{"Frente (Shiny)"}</p>
-          </div>
-          <div>
-          <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
-          </div>
-        </div>
-        {pokemon.sprites.back_shiny !== null && 
-        <div className="img2">
-          <div className="nomePok">
-            <p className="lado">{"Costas (Shiny)"}</p>
-          </div>
-          <div>
-          <img src={pokemon.sprites.back_shiny} alt={pokemon.name} />
-          </div>
-        </div>}
       </div>
-      {pokemon.sprites.back_shiny_female != null && 
-        <div className="img1">
-          <div className="img2">
-            <div className="nomePok">
-              <p className="lado">{'Frente (Fêmea Shiny)'}</p>
+      <div className="img3">
+        {pokemon.sprites.back_female != null && 
+          <div className="img1">
+            <div className="img2">
+              <div className="nomePok">
+                <p className="lado">{'Frente (Fêmea)'}</p>
+              </div>
+              <div>
+                <img src={pokemon.sprites.front_female} alt={pokemon.name} />
+              </div>
             </div>
-            <div>
-              <img src={pokemon.sprites.front_shiny_female} alt={pokemon.name} />
+            <div className="img2">
+              <div className="nomePok">
+                <p className="lado">{"Costas (Fêmea)"}</p>
+              </div>
+              <div>
+                <img src={pokemon.sprites.back_female} alt={pokemon.name} />
+              </div>
             </div>
-          </div>
-          <div className="img2">
-            <div className="nomePok">
-              <p className="lado">{"Costas (Fêmea Shiny)"}</p>
+        </div> }
+        {pokemon.sprites.back_shiny_female != null && 
+          <div className="img1">
+            <div className="img2">
+              <div className="nomePok">
+                <p className="lado">{'Frente (Fêmea Shiny)'}</p>
+              </div>
+              <div>
+                <img src={pokemon.sprites.front_shiny_female} alt={pokemon.name} />
+              </div>
             </div>
-            <div>
-              <img src={pokemon.sprites.back_shiny_female} alt={pokemon.name} />
+            <div className="img2">
+              <div className="nomePok">
+                <p className="lado">{"Costas (Fêmea Shiny)"}</p>
+              </div>
+              <div>
+                <img src={pokemon.sprites.back_shiny_female} alt={pokemon.name} />
+              </div>
             </div>
-          </div>
-      </div> }
+        </div> }
+      </div>
     </div>
   )
 }
